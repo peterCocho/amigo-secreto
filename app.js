3e342c784c9ce7d94370371e535ae30c05ea831c
Peter Stranger <petercocho505.com>
2025-02-03 19:11:51 -0400

    renombrar la carpeta challenge-amigo-secreto_esp-main  a amigo-secreto

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// creando el arry amigos para almacenarlos
+let amigos = []
+
+// Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+function agregarAmigo() {
+  let amigo = document.getElementById('amigo').value;
+  if (amigo != "") {
+    amigos.push(amigo);
+    console.log(amigos)
+    limpiarCampo()
+  } else {
+    alert("Por favor, inserte un nombre.")
+  }
+  
+
+}
+
+function limpiarCampo() {
+  document.querySelector('#amigo').value = '';
+}
+
